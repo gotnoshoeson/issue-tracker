49fc4791bc19315ee7796b69b33691106ec91855
gotnoshoeson <miles_patterson@icloud.com>
2023-11-11 16:29:50 -0800

    Refactor: move inline function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,7 +10,7 @@ import { useRouter } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchema";
 import { z } from 'zod';
-import ErrorMessage from "@/app/components/errorMessage";
+import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
 
 type IssueForm = z.infer<typeof createIssueSchema>
@@ -23,6 +23,18 @@ const NewIssuePage = () => {
   const [error, setError] = useState('');
   const [isSubmitting, setIsSubmitting] = useState(false);
 
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setIsSubmitting(true);
+      await axios.post('/api/issues', data);
+      router.push('/issues');
+      
+    } catch (error) {
+      setIsSubmitting(false);
+      setError('An unexpected error occurred.')
+    }
+  })
+
   return (
     <div className="max-w-xl">
       {error && <Callout.Root color='red' className="mb-5">
@@ -30,17 +42,7 @@ const NewIssuePage = () => {
         </Callout.Root>}
       <form
         className='space-y-3'
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setIsSubmitting(true);
-            await axios.post('/api/issues', data);
-            router.push('/issues');
-            
-          } catch (error) {
-            setIsSubmitting(false);
-            setError('An unexpected error occurred.')
-          }
-        })}>
+        onSubmit={onSubmit}>
         <TextField.Root>
           <TextField.Input placeholder='Title' {...register("title")}/>
         </TextField.Root>
