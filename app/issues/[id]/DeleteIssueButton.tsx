48e926b8e0538c5e6bd9fb27822223254c15e8d9
gotnoshoeson <miles_patterson@icloud.com>
2023-11-13 17:25:58 -0800

    Show dialog confirmation to delete

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,9 +1,27 @@
-import { Button } from '@radix-ui/themes'
+'use client'
+
+import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 import React from 'react'
 
 const DeleteIssueButton = ({ issueId }: { issueId: number } ) => {
   return (
-    <Button color='red'>Delete</Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color='red'>Delete</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Delete Issue</AlertDialog.Title>
+        <AlertDialog.Description>Are you sure you want to delete? This action cannot be undone.</AlertDialog.Description>
+        <Flex className='mt-4' gap="3">
+          <AlertDialog.Cancel>
+            <Button variant='soft' color='gray'>Cancel</Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color='red'>Delete</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   )
 }
 
