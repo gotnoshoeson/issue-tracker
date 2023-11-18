6a8fae540603bd350cdc0f6fa690a8eee5520697
gotnoshoeson <miles_patterson@icloud.com>
2023-11-18 12:09:37 -0800

    Refactor: AssigneeSelect component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -9,26 +9,26 @@ import { Skeleton } from '@/app/components';
 import toast, { Toaster } from 'react-hot-toast';
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-  const { data: users, error, isLoading } = useQuery<User[]>({
-    queryKey: ['users'],
-    queryFn: () => axios.get('/api/users').then(res => res.data),
-    staleTime: 60 * 1000, // 60s
-    retry: 3
-  });
+  const { data: users, error, isLoading } = useUsers();
 
   if (isLoading) return <Skeleton></Skeleton>
 
   if (error) return null;
 
+  const assignIssue = async (userId: string) => {
+    try {
+      await axios.patch('/api/issues/' + issue.id, { assignedToUserId: userId || null });
+    } catch (error) {
+      toast.error('Changes could not be saved.')
+    }
+  }
+
   return (
     <>
-      <Select.Root defaultValue={issue.assignedToUserId || ""}  onValueChange={async (userId) => {
-        try {
-          await axios.patch('/api/issues/' + issue.id, { assignedToUserId: userId || null });
-        } catch (error) {
-          toast.error('Changes could not be saved.')
-        }
-      }}>
+      <Select.Root
+        defaultValue={issue.assignedToUserId || ""} 
+        onValueChange={assignIssue}
+      >
         <Select.Trigger placeholder='Select...'></Select.Trigger>
         <Select.Content>
           <Select.Group>
@@ -42,6 +42,13 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
       <Toaster />
     </>
   )
-}
+};
+
+const useUsers = () => useQuery<User[]>({
+  queryKey: ['users'],
+  queryFn: () => axios.get('/api/users').then(res => res.data),
+  staleTime: 60 * 1000, // 60s
+  retry: 3
+});
 
 export default AssigneeSelect
